5887855103fb175a6b5233a084808e577442972b
victor dias <victor.dias@meumobi.com>
2017-09-08 18:50:01 +0100

    ENHANCE: Closes #69, add provider as argument to command providers:perform

--- app/Console/Commands/ProvidersPerform.php
+++ app/Console/Commands/ProvidersPerform.php
@@ -8,44 +8,51 @@ use App\Jobs\DailyReportsPublishing;
 class ProvidersPerform extends Command
 {
     
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'providers:perform';
+  /**
+    * The name and signature of the console command.
+    *
+    * @var string
+    */
+  protected $signature = 'providers:perform {provider?}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+  /**
+    * The console command description.
+    *
+    * @var string
+    */
+  protected $description = 'Command description';
 
-    private $providers = ["Adserving", "Adsense", "Adtech", "Criteo", "Rubicon", "Unplugged"];
-    //private $providers = ["Rubicon"];
+  private $providers = ["Adserving", "Adsense", "Adtech", "Criteo", "Rubicon", "Unplugged"];
+  //private $providers = ["Rubicon"];
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        
+  /**
+    * Create a new command instance.
+    *
+    * @return void
+    */
+  public function __construct()
+  {
+      parent::__construct();
+      
+  }
+
+  /**
+    * Execute the console command.
+    *
+    * @return mixed
+    */
+  public function handle()
+  {
+    $providerName = $this->argument('provider');
+
+    if ($providerName) {
+      $this->info('Commands: Perform ' . $providerName);
+      dispatch(new DailyReportsPublishing($providerName, "console"));
+    } else {
+      foreach ($this->providers as $providerName) {
+        $this->info('Commands: Perform ' . $providerName);
+        dispatch(new DailyReportsPublishing($providerName, "console"));
+      }
     }
-
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        foreach ($this->providers as $providerName) {
-            $this->info('Commands: Perform ' . $providerName);
-            dispatch(new DailyReportsPublishing($providerName, "console"));
-        }
-    }
+  }
 }
